0c76a789dedeb4bf989b0c9df5267b89cb53c160
unknown <vrnakano@gmail.com>
2020-12-07 15:46:02 -0300

    Implementação ao da página cadastro e login, segurança, rotas

--- projeto-aspas/src/app/service/auth.service.ts
+++ projeto-aspas/src/app/service/auth.service.ts
@@ -7,17 +7,38 @@ import { UsuarioModel } from '../model/UsuarioModel';
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthService {
 
   constructor(private http: HttpClient) { }
 
-  logar (userLogin: UserLogin): Observable<UserLogin> { 
+  logar(userLogin: UserLogin): Observable<UserLogin> {
     return this.http.post<UserLogin>('http://localhost:8081/usuario/logar', userLogin)
 
   }
 
-  cadastrar (user: UsuarioModel): Observable<UsuarioModel> {
+  cadastrar(user: UsuarioModel): Observable<UsuarioModel> {
     return this.http.post<UsuarioModel>('http://localhost:8081/usuario/cadastrar', user)
+  }
+
+  btnSair() {
+
+    let ok = false;
+    let token = localStorage.getItem('token')
+    if (token != null) {
+      ok = true
+    }
+    return ok
+  }
+
+  btnLogin() {
+    let ok = false;
+    let token = localStorage.getItem('token')
+
+    if (token == null) {
+      ok = true
+    }
+    return ok
   }
 
 }
