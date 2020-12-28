d09e29c8568fe437f6e5520cbd23ca085c5a7474
unknown <chris_amantino97@hotmail.com>
2020-12-28 11:46:36 -0300

    salvar nome e tipo em localStorage

--- projeto-aspas/src/app/model/UserLogin.ts
+++ projeto-aspas/src/app/model/UserLogin.ts
@@ -1,7 +1,9 @@
 export class UserLogin {
 
+  public nomeCompleto!: string;
   public emailUsuario!: string;
   public senhaUsuario!: string;
+  public tipoUsuario!: string;
   public token!: string;
 
 }
